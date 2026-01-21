37e42e3fd338b9ba58f5401373068338c51015a7
Pavel Zabelin <zabelin.pavel.work@gmail.com>
2026-01-21 14:41:12 +0100

    feat: add 'Arithmetic Progression' game (brain-progression)

---
+++ bin/games/progression-rules.js
@@ -0,0 +1,20 @@
+import getRandomNumber from '../../src/utils.js';
+
+const getDescription = () => 'What number is missing in the progression?';
+
+const getRoundData = () => {
+  const progressionSize = getRandomNumber(5, 10);
+  const progressionStep = getRandomNumber(1, 10);
+  const progressionStart = getRandomNumber();
+  const progression = [progressionStart];
+  for (let i = 1; i < progressionSize; i += 1) {
+    progression.push(progressionStart + progressionStep * i);
+  }
+  const missingItemIndex = getRandomNumber(0, progressionSize - 1);
+  const answer = progression[missingItemIndex];
+  progression[missingItemIndex] = '..';
+
+  return [`Question: ${progression.join(' ')}`, String(answer)];
+};
+
+export default { getDescription, getRoundData };
