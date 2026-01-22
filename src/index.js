538041b7a196b888dbf9aba7ae496d20524f424a
Pavel Zabelin <zabelin.pavel.work@gmail.com>
2026-01-22 14:26:12 +0100

    fix error  Closing curly brace appears

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const runGame = (gameRules) => {
     const userAnswer = readlineSync.question('Your answer: ')
     if (userAnswer === answer) {
       console.log('Correct!')
-    } else {
+    }
+    else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`)
       console.log(`Let's try again, ${name}!`)
       return
