a6d3412b10719a6f376f141b7d6d5120783971ed
Pavel Zabelin <zabelin.pavel.work@gmail.com>
2026-01-22 13:53:06 +0100

    name changing to brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import gameRules from './games/calculator-rules.js';
+import gameRules from './games/calc-rules.js';
 import runGame from '../src/index.js';
 
 runGame(gameRules);
